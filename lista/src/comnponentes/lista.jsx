c0112805fbfe45f700f5f99b14a430a26d2dee10
Rômulo Martins <romulodev08@gmail.com>
2022-01-12 14:16:54 -0300

    informações dos pokemons salvas

--- lista/src/comnponentes/lista.jsx
+++ lista/src/comnponentes/lista.jsx
@@ -5,25 +5,38 @@ export default class Lista extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            response: []
+            response: [],
+            dadosDosPokemons: ['']
         }
     }
     componentDidMount() {
-        axios.get("https://pokeapi.co/api/v2/pokemon/?offset=20&limit=20")
-            .then(resposta => {
-                const dados = resposta.data.results
-                this.setState({ response: dados })
+        axios.get("https://pokeapi.co/api/v2/pokemon/?offset=20&limit=20").then(resposta => {
+            const dados = resposta.data.results
+            this.setState({ response: dados })
+        })
+        setTimeout(() => {
+            let dados = []
+            this.state.response.map((item, index) => {
+                axios.get(item.url).then( res => {
+                    dados[index] = res.data
+                })
             })
+            this.setState({dadosDosPokemons: dados})
+            console.log(this.state.dadosDosPokemons)
+        })
     }
     render() {
         //console.log(this.state.response)
+        //console.log(this.state.dadosDosPokemons)
         return (
             <div>
-                {this.state.response.map(item => <ul key={item.name} >
-                    <li>{item.name}</li>
-                    <li><a target="_blank" href={item.url}>{item.url}</a></li>
-                </ul>)}
-            </div>
+                {
+                    this.state.response.map((item, index) => <ul key={index} >
+                        <li>{item.name}</li>
+                        <li><a target="_blank" href={item.url}>{item.url}</a></li>
+                    </ul>)
+                }
+            </div >
         )
     }
 }
