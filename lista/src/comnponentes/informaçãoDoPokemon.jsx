a6459109dc682d95386a34e138b978ada3a5b920
Rômulo Martins <romulodev08@gmail.com>
2022-01-13 12:31:43 -0300

    ligação de informação entre componentes concluido

--- lista/src/comnponentes/informaçãoDoPokemon.jsx
+++ lista/src/comnponentes/informaçãoDoPokemon.jsx
@@ -6,20 +6,34 @@ export default class PokemonInfo extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            infoDoPokemon: []
+            chave: null,
+            nome: '',
         }
     }
-    componentDidMount() {
-        this.setState({ infoDoPokemon: this.props.dados })
-    }
     componentDidUpdate() {
-        //console.log(this.state.infoDoPokemon)
+        //passando os dados do props (dados do pokémon selecionado) para o state desse componente
+        if (this.props.dados != undefined /*se os dados do pokemon nao for inderfinido (no caso, o valor inicial é indefinnido pois nenhum pokemon é selecionado) */ && this.props.chave != this.state.chave /*caso o componente ja tenha sido atualizado, a chave do props e a chave do state serão a mesma, então não irá executar a ação de resetetar o state com novas informações e gerar um ciclo infinito*/) {
+            this.setState({
+                chave: this.props.chave,
+                nome: this.props.dados.name,
+            })
+            setTimeout(() => {
+                console.log(this.props.dados)
+                console.log(this.state.chave)
+                console.log(this.state.nome)
+            })
+        }
     }
     render() {
-        return(
+        return (
             <section className="telaDeInfoDoPokemon">
-                <p>{this.props.chave}</p>
-                <p> {  } </p>
+                <h2></h2> <div className="imgPokemon"><img src="" alt="" /></div>
+                <h3>habilidades:</h3>
+                <p>nome de cada habilidade n linhas</p>
+                <h3>Experiência base:</h3>
+                <p>numero</p>
+
+
             </section>
         )
     }
--- lista/src/comnponentes/listaDosPokemons.jsx
+++ lista/src/comnponentes/listaDosPokemons.jsx
@@ -8,32 +8,38 @@ export default class Lista extends Component {
         this.state = {
             response: [],
             chave: null,
-            infoPokemon: []
+            infoPokemons: [],
+            switchPokemon: []
         }
     }
     componentDidMount() {
         axios.get("https://pokeapi.co/api/v2/pokemon/?offset=20&limit=20").then(resposta => {
             const dados = resposta.data.results
             this.setState({ response: dados })
+            //console.log(this.state.response)
         })
         setTimeout(() => {
             let dados = []
             this.state.response.map((item, index) => {
-                axios.get(item.url).then( res => {
+                axios.get(item.url).then(res => {
                     dados[index] = res.data
                 })
             })
-            this.setState({ infoPokemon: dados })
-            console.log(this.state.infoPokemon)     
+            this.setState({ infoPokemons: dados })
+            //console.log(this.state.infoPokemons)
         })
+
+    }
+    atualizar(chave) {
+        this.setState({ chave: chave, switchPokemon: this.state.infoPokemons[chave] })
     }
     render() {
         return (
             <div>
-                <PokemonInfo chave={this.state.chave} dados={this.state.infoPokemon} />
+                <PokemonInfo chave={this.state.chave} dados={this.state.switchPokemon} />
                 {
-                    this.state.response.map((item, index) => 
-                    <div key={index} onClick={() => this.setState({ chave: index })} >{item.name}</div>)
+                    this.state.response.map((item, index) =>
+                        <div key={index} onClick={() => this.atualizar(index)} >{item.name}</div>)
                 }
             </div >
         )
